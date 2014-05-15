923d8e94cfe692893965ec39376935d72104eedf
clement <clement@evaneos.com>
2014-05-15 17:03:49 +0200

    removing demoJson

--- demo/app/controllers/Tab3Controller.js
+++ demo/app/controllers/Tab3Controller.js
@@ -1,10 +1,5 @@
 
 angular.module('demo')
-    .filter('demoJson',  function($filter) {
-        return function(obj) {
-            return JSON.stringify(obj, null, 2);
-        }
-    })
     .controller('Tab3Controller', [ '$scope', 'DemoContextService', function($scope, demoContext) {
 
     $scope.context = demoContext;
