09c5b8442964de0e8c360a9db7fa23dcde085019
clement <clement@evaneos.com>
2014-05-15 18:28:12 +0200

    buggy css generation

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,12 +20,42 @@ module.exports = function(grunt) {
           'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
         }
       }
-    }
+    },
+    less: {
+        options: {
+                paths: [
+                    'less',
+                    '.'
+                ]
+        },
+        production: {
+            options: {
+                compress: true
+            },
+            src: "less/generators/production.less",
+            dest: "dist/ev-fdm.min.css"
+        },
+        // vendors: {
+        //     options: {
+        //         outputSourceFiles: true,
+        //         sourceMap: true,
+        //         sourceMapFilename: 'dist/vendors.css.map',
+        //         sourceMapURL: 'dist/vendors.css.map'
+        //     },
+        //     src: 'less/vendors.less',
+        //     dest: 'dist/vendors.css'
+        // }
+    },
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['concat', 'uglify']);
+  grunt.registerTask('js', ['concat', 'uglify']);
+  grunt.registerTask('css', [/*'less:development',*/ 'less:production' /*, 'less:vendors'*/]);
+
+  grunt.registerTask('default', ['js', 'uglify']);
 
 };
--- README.md
+++ README.md
@@ -2,3 +2,37 @@ ev-fdm
 ======
 
 Framework de Malade / Framework de Merde / Faute de Mieux
+
+This is the JS and CSS framework used for the backoffice of Evaneos.com
+
+## Architecture of the repository
+
+This repository contains the following things:
+
+ - the main framework code, in the js and less directories
+ - the compiled files, in the dist directory
+
+## Requirements
+
+If you simply want to use the framework, you simly need to have bower installed, as you can see in the demo.
+
+However if you want to recompile the sources, this framework require the following applications to be installed :
+
+ - node
+ - npm
+ - grunt
+ - bower
+
+## Using the demo
+
+in the demo repository, execute
+
+    bower install
+
+and you are good to go !
+
+## Compiling the framework
+
+At the root of the project, run
+
+    bower install && grunt
