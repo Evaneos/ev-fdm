2944d72d0c76d6e5c4d217ad2d26fb77659512f6
SelrahcD <charles@evaneos.com>
2014-07-02 18:22:26 +0200

    Add disable validation directive

--- dist/core/ev-fdm-core.js
+++ dist/core/ev-fdm-core.js
@@ -2874,6 +2874,31 @@ angularLocalStorage.service('localStorageService', [
 
 }]);
 angular.module('ev-fdm')
+  .directive('disableValidation', function() {
+    return {
+      require: '^form',
+      restrict: 'A',
+      link: function(scope, element, attrs, form) {
+        var control;
+
+        scope.$watch(attrs.disableValidation, function(value) {
+          if (!control) {
+            control = form[element.attr("name")];
+          }
+          if (value == false) {
+            form.$addControl(control);
+            angular.forEach(control.$error, function(validity, validationToken) {
+              form.$setValidity(validationToken, !validity, control);
+            });
+          } else {
+            form.$removeControl(control);
+          }
+        });
+      }
+    };
+  });
+
+angular.module('ev-fdm')
 .directive('strictMin', function() {
     return {
         require: 'ngModel',
