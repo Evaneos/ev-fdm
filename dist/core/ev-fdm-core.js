5316f66e6a33332c26a82ca9c1155f7579d88cdc
johan <johan@evaneos.com>
2014-07-16 15:23:53 +0200

    Adding selectLanguage directive to ev-fdm

--- dist/core/ev-fdm-core.js
+++ dist/core/ev-fdm-core.js
@@ -1017,6 +1017,37 @@ angular.module('ev-fdm').directive('body', ['$rootScope', '$state', function ($r
         }
     };
 }]);
+angular.module('ev-fdm')
+    .provider('evSelectLanguage', function() {
+        this.$get =function () {
+            return {languages: this.languages || []};
+        };
+
+        this.setLanguages =function (languages) {
+            this.languages = languages;
+        };
+    })
+    .directive('evSelectLanguage', ['evSelectLanguage', function (cfg) {
+        return {
+            template:
+                '<div class="ev-language-tabs">' +
+                    '<div class="btn-group">' +
+                        '<button class="btn" ng-repeat="lang in languages" ng-class="{active: selectedLang===lang}"' +
+                            'ng-click="$parent.selectedLang=lang">' +
+                            '<span class="ev-icons-flags" ng-class="\'icon-\' + lang"></span>' +
+                        '</button>' +
+                    '</div>' +
+                '</div>',
+            restrict: 'AE',
+            scope: {
+                selectedLang: '=lang'
+            },
+            link: function($scope) {
+                $scope.languages = cfg.languages;
+            }
+        };
+    }]);
+
 'use strict';
 /// This directive currently depend on ng-repeat $index for the
 ///  shift selection. It would be great to remove this depency.
