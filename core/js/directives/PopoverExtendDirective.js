2619498c951c91ec7398b39335b11547792c6566
johan <johan@evaneos.com>
2014-07-24 14:52:21 +0200

    Popover: now they can have (safe) html inside !

--- core/js/directives/PopoverExtendDirective.js
+++ core/js/directives/PopoverExtendDirective.js
@@ -1,5 +1,18 @@
 (function () {
     'use strict';
+        // update popover template for binding unsafe html
+    angular.module("template/popover/popover.html", []).run(["$templateCache", function ($templateCache) {
+        $templateCache.put("template/popover/popover.html",
+          "<div class=\"popover {{placement}}\" ng-class=\"{ in: isOpen(), fade: animation() }\">\n" +
+          "  <div class=\"arrow\"></div>\n" +
+          "\n" +
+          "  <div class=\"popover-inner\">\n" +
+          "      <h3 class=\"popover-title\" ng-bind-html=\"title\" ng-show=\"title\"></h3>\n" +
+          "      <div class=\"popover-content\"ng-bind-html=\"content\"></div>\n" +
+          "  </div>\n" +
+          "</div>\n" +
+          "");
+    }]);
     angular.module('ev-fdm')
         .directive('popover', ['$timeout', function ($timeout) {
         	return {
--- dist/ev-fdm.js
+++ dist/ev-fdm.js
@@ -847,6 +847,19 @@ var module = angular.module('ev-fdm')
 }]);
 (function () {
     'use strict';
+        // update popover template for binding unsafe html
+    angular.module("template/popover/popover.html", []).run(["$templateCache", function ($templateCache) {
+        $templateCache.put("template/popover/popover.html",
+          "<div class=\"popover {{placement}}\" ng-class=\"{ in: isOpen(), fade: animation() }\">\n" +
+          "  <div class=\"arrow\"></div>\n" +
+          "\n" +
+          "  <div class=\"popover-inner\">\n" +
+          "      <h3 class=\"popover-title\" ng-bind-html=\"title\" ng-show=\"title\"></h3>\n" +
+          "      <div class=\"popover-content\"ng-bind-html=\"content\"></div>\n" +
+          "  </div>\n" +
+          "</div>\n" +
+          "");
+    }]);
     angular.module('ev-fdm')
         .directive('popover', ['$timeout', function ($timeout) {
         	return {
