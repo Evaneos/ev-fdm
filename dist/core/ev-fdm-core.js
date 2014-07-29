067134f2ac815e1311457c04a4e7fa4f4a8a94f2
johan <johan@evaneos.com>
2014-07-29 18:33:33 +0200

    Fix fixed-header directive

--- dist/core/ev-fdm-core.js
+++ dist/core/ev-fdm-core.js
@@ -495,7 +495,7 @@ module.directive('evFilters', function() {
         .directive('evFixedHeader', function () {
             return {
                 link: function($scope, $element, $attrs) {
-
+                    $element.addClass('ev-full-height');
                     var header = $element.find('>.ev-header');
                     var body   = $element.find('>.ev-body');
                     body.css({'overflow-y': 'auto'});
@@ -627,45 +627,6 @@ angular.module('ev-fdm')
         }
     }
 }]);
-(function () {
-    'use strict';
-    angular.module('ev-fdm')
-        .directive('evFullHeightBody', function () {
-            return {
-                link: function($scope, $element, $attrs) {
-                    $element.addClass('ev-full-height');
-
-                    var header = $element.find('>.ev-full-height-body-header');
-                    var body   = $element.find('>.ev-full-height-body');
-
-                    var refreshDimensions = function() {
-                        body.hide();
-                        var bodyHeight = $element.innerHeight() - header.outerHeight(true);
-
-                        body.show();
-                        body.height(bodyHeight);
-
-                        if ($attrs.refreshIdentifier) {
-                            $scope.$broadcast('evFullHeightBody::refresh::' + $attrs.refreshIdentifier);
-                        }
-                    };
-
-
-                    $scope.$watch(function() {
-                        return $element.height() + header.outerHeight(true);
-                    }, refreshDimensions);
-
-                    $(window).bind('resize', refreshDimensions);
-
-                    if ($attrs.refreshOn) {
-                        $scope.$on('evFullHeightBody::refresh::' + $attrs.refreshOn, refreshDimensions);
-                    }
-
-                }
-            };
-        });
-}) ();
-
 'use strict';
 
 angular.module('ev-fdm')
