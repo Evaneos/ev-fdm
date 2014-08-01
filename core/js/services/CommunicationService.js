f7b7bd9cfa1bde27acab668bf66a25ea8688a035
cyrilf <cyrilf7x@gmail.com>
2014-08-01 17:43:30 +0200

    CommunicationService implemented

---
+++ core/js/services/CommunicationService.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var module = angular.module('ev-fdm');
+
+/**
+ * Communication Service
+ * Manage the communication for our app
+ */
+module.service('communicationService', ['$rootScope', function($rootScope) {
+
+    var COMMUNICATION_KEY = 'evfdm-communication';
+
+    /**
+     * Emit an event
+     */
+    var emit = function(eventName, params) {
+        $rootScope.$emit(eventName, params);
+    };
+
+    /**
+     * Listen to an event
+     */
+    var on = function(eventName, callback) {
+        $rootScope.$on(eventName, callback);
+    };
+
+    /**
+     * Set a key/value
+     */
+    var set = function(key, value) {
+        if($rootScope[COMMUNICATION_KEY] === undefined) {
+            $rootScope[COMMUNICATION_KEY] = {};
+        }
+
+        $rootScope[COMMUNICATION_KEY][key] = value;
+    };
+
+    /**
+     * Get a value by key
+     */
+    var get = function(key) {
+        var result = null;
+        if($rootScope[COMMUNICATION_KEY] && $rootScope[COMMUNICATION_KEY][key] !== undefined) {
+            result = $rootScope[COMMUNICATION_KEY][key];
+        }
+
+        return result;
+    };
+
+    var communicationService = {
+        emit: emit,
+        on  : on,
+        set : set,
+        get : get
+    };
+
+    return communicationService;
+}]);
--- dist/core/ev-fdm-core.js
+++ dist/core/ev-fdm-core.js
@@ -2017,6 +2017,64 @@ angular.module('ev-fdm')
     }]);
 'use strict';
 
+var module = angular.module('ev-fdm');
+
+/**
+ * Communication Service
+ * Manage the communication for our app
+ */
+module.service('communicationService', ['$rootScope', function($rootScope) {
+
+    var COMMUNICATION_KEY = 'evfdm-communication';
+
+    /**
+     * Emit an event
+     */
+    var emit = function(eventName, params) {
+        $rootScope.$emit(eventName, params);
+    };
+
+    /**
+     * Listen to an event
+     */
+    var on = function(eventName, callback) {
+        $rootScope.$on(eventName, callback);
+    };
+
+    /**
+     * Set a key/value
+     */
+    var set = function(key, value) {
+        if($rootScope[COMMUNICATION_KEY] === undefined) {
+            $rootScope[COMMUNICATION_KEY] = {};
+        }
+
+        $rootScope[COMMUNICATION_KEY][key] = value;
+    };
+
+    /**
+     * Get a value by key
+     */
+    var get = function(key) {
+        var result = null;
+        if($rootScope[COMMUNICATION_KEY] && $rootScope[COMMUNICATION_KEY][key] !== undefined) {
+            result = $rootScope[COMMUNICATION_KEY][key];
+        }
+
+        return result;
+    };
+
+    var communicationService = {
+        emit: emit,
+        on  : on,
+        set : set,
+        get : get
+    };
+
+    return communicationService;
+}]);
+'use strict';
+
 // Map that stores the selected filters across pages
 angular.module('ev-fdm').
     service('FilteringService', ['$location', function ($location) {
