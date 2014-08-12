34416a48a20433c17b428dadc4ef6e5f82c93510
Johan <johangirod@gmail.com>
2014-08-12 17:29:15 +0200

    sumFilter now return 0 if value is undefined

--- core/js/filters/SumFilter.js
+++ core/js/filters/SumFilter.js
@@ -1,6 +1,9 @@
 angular.module('ev-fdm')
      .filter('sum', ['$parse', function($parse) {
             return function(objects, key) {
+                if (!angular.isDefined(objects)) {
+                    return 0;
+                }
                 var getValue = $parse(key);
                 return objects.reduce(function(total, object) {
                     var value = getValue(object);
