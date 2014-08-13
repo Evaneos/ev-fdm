a0d942d83340b424c6194b79128b324f2ceef6c2
remievaneos <remi@evaneos.com>
2014-08-13 07:20:58 -0700

    modified upload directive to support more use cases

--- plugins/upload/js/directives/uploadDirective.js
+++ plugins/upload/js/directives/uploadDirective.js
@@ -8,11 +8,20 @@
         =========
         Hi! I'm a directive used for uploading files.
 
-        You can give me two callback: `uploadStart` and `fileSuccess`
+        You can give me three callback: `uploadStart`, `fileSuccess` and `fileAdded`
+        - `uploadStart` will be called when a new multiple upload start (for instance, when the user dropped some files
+            on the dropzone). It will be call with an argument: the promise for the status of the whole upload.
         - `fileSuccess` will be called each time a file has successfully been uploaded, with the data returned by the
             server.
-        - `upload` will be called when a new multiple upload start (for instance, when the user dropped some files
-            on the dropzone). It will be call with an argument: the promise for the status of the whole upload.
+        - `fileAdded` will be called each time a file is added to the queue. It will be called with 2 arguments :
+        	- dropzoneFile : the Dropzone file being uploaded
+        	- promise : the promise associated with the file
+
+		Clickable Element : you can define a clickable element inside the directive with the
+							class '.ev-upload-clickable'
+		
+		Dropzone Element : you can define a clickable element inside the directive with the class '.ev-upload-dropzone'
+						   If the class is not present, it will use the root element. 
 
         My inner heart is powered by Dropzone. You can pass any settings to it through my `settings` parameter.
         Consequently, you can do whatever you want. Be wise :)
@@ -39,23 +48,40 @@
                 scope: {
                     settings: '=',
                     uploadStart: '&upload',
-                    fileSuccess: '&'
+                    fileSuccess: '&',
+                    fileAdded: '&'
                 },
-                template: '<div class="ev-upload"><div class="dz-default dz-message" ng-transclude> </div></div>',
+                template: '<div class="ev-upload"><div ng-transclude> </div></div>',
                 link: function ($scope, elem, attrs) {
 
+                	$scope.fileSuccess = $scope.fileSuccess || function() {};
+                	$scope.fileAdded = $scope.fileAdded || function() {};
+                	
                     var dropzone = null;
                     var progress = null;
 
-                    var clickableZone = elem.find('.ev-upload-clickable')[0];
-
+                    var filesPromises = {};
+                    
                     function getBytes (status) {
                         return dropzone.getAcceptedFiles().reduce(function (bytes, file) {
                             return bytes + file.upload[status];
                         }, 0);
                     }
 
-
+                    function getDropzoneElement() {
+                    	var dz = elem.find('.ev-upload-dropzone');
+                    	if (dz.length === 0) {
+                    		dz = elem;
+                    	}
+                    	dz.addClass("dz-default");
+                    	dz.addClass("dz-message");
+                    	return dz[0];
+                    }
+                    
+                    function getClickableElement() {
+                    	return elem.find('.ev-upload-clickable')[0];
+                    } 
+                    
                     $scope.$watch('settings', function (settings) {
                         if (!settings.url) {
                             $log.warn('No url provided to the upload zone');
@@ -65,7 +91,10 @@
                             dropzone.destroy();
                         }
                         settings = angular.extend(BASE_CONFIG, settings);
-                        dropzone = new Dropzone(elem[0], angular.extend({clickable: clickableZone}, settings));
+                        dropzone = new Dropzone(
+                    		getDropzoneElement(),
+                    		angular.extend({clickable: getClickableElement()},settings)
+                		);
                         // the promise for the whole upload
 
                         $scope.currentUpload = null;
@@ -73,15 +102,37 @@
                         // At the beginning of a new file upload.
                         dropzone.on('sending', function (file) {
                             if ($scope.currentUpload === null) {
-                                $scope.$apply(startNewUpload);
+                            	var deferred = $q.defer();
+                            	filesPromises[file.fullPath] = deferred;
+                            	$scope.fileAdded({dropzoneFile: file, promise: deferred.promise});
+                            	
+                            	$scope.$apply(startNewUpload);
                             }
+                        });
+                        
+                        dropzone.on('uploadprogress', function (file, progress) {
+                        	$scope.$apply(function ($scope) {
+                            	filesPromises[file.fullPath].notify(progress);
+                            });
                         });
 
                         dropzone.on('success', function (file, response) {
-                            progress.done += 1;
                             $scope.$apply(function ($scope) {
+                            	filesPromises[file.fullPath].resolve({file: response});
+                            	
                                 $scope.fileSuccess({file: response});
                             });
+                        });
+                        
+                        dropzone.on('error', function (file, response) {
+                            $scope.$apply(function ($scope) {
+                            	filesPromises[file.fullPath].reject({errorMessage: response});
+                            });
+                        });
+                        
+                        dropzone.on('complete', function (file) {
+                        	progress.done += 1;
+                        	delete filesPromises[file.fullPath];
                         });
 
                     }, true);
