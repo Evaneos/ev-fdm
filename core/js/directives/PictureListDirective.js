2fa74dbdc731a0ee0afb0446aac99d6fe5da1684
Christophe Hurpeau <christophe@hurpeau.com>
2014-09-26 18:00:59 +0200

    use ng-src in PictureList directive

--- core/js/directives/PictureListDirective.js
+++ core/js/directives/PictureListDirective.js
@@ -18,7 +18,7 @@
                     '<li ng-repeat="picture in pictures track by picture.id" class="col-xs-{{colNumberBootstrap}} ev-animate-picture-list">' +
                         '<figure>' +
                             '<div class="picture-thumb">' +
-                                '<img src="{{picture.id | imageUrl:245:150 | escapeQuotes }}" />' +
+                                '<img ng-src="{{picture.id | imageUrl:245:150 | escapeQuotes }}" />' +
                                 '<button class="action update-action ev-upload-clickable"' +
                                     'ng-click="onUpdate({picture: picture, index: $index})" ' +
                                     'data-ng-show="editable && showUpdate">' +
