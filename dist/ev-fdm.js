96d01edc2db978c89da3ef4c19c84d451d526f26
Christophe Hurpeau <christophe@hurpeau.com>
2014-10-15 19:02:02 +0200

    [Generating dist files]

--- dist/ev-fdm.js
+++ dist/ev-fdm.js
@@ -1871,7 +1871,6 @@ angular.module('ev-fdm')
                     };
 
                     $scope.isShowed = function (pane) {
-    
                         return pane.alwaysShow || !!pane.tabShow;
                     };
                 },
@@ -4338,6 +4337,60 @@ angular.module('ev-tinymce', [])
         };
     }]);
 }) (window.tinyMCE);
+
+/**
+ * plugin.js
+ *
+ * Copyright, Moxiecode Systems AB
+ * Released under LGPL License.
+ *
+ * License: http://www.tinymce.com/license
+ * Contributing: http://www.tinymce.com/contributing
+ */
+
+/*global tinymce:true */
+
+tinymce.PluginManager.add('evimage', function(editor) {
+    function showDialog() {
+        var data, dom = editor.dom, imgElm = editor.selection.getNode();
+
+        if (imgElm) {
+            data = {
+                src: dom.getAttrib(imgElm, 'src'),
+                alt: dom.getAttrib(imgElm, 'alt'),
+                "class": dom.getAttrib(imgElm, 'class'),
+                dataPictureId: dom.getAttrib(imgElm, 'data-picture-id')
+            };
+        } else {
+            data = false;
+        }
+
+        editor.settings.evimages(data, function(newAttributes) {
+            if (imgElm) {
+                dom.setAttribs(imgElm, newAttributes);
+            } else {
+                editor.insertContent(dom.create('img', newAttributes));
+            }
+        });
+    }
+
+    editor.addButton('evimage', {
+        icon: 'image',
+        tooltip: 'Insert/edit image',
+        onclick: showDialog,
+        stateSelector: 'img[data-picture-id]:not([data-mce-object],[data-mce-placeholder])'
+    });
+
+    editor.addMenuItem('evimage', {
+        icon: 'image',
+        text: 'Insert image',
+        onclick: showDialog,
+        context: 'insert',
+        prependToContext: true
+    });
+
+    editor.addCommand('mceImage', showDialog);
+});
 
 (function () {
     'use strict';
