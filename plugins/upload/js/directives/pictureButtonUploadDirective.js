b6b609be7422d52fc5ba5dc84de97a70d98b1e61
cyrilf <cyrilf7x@gmail.com>
2014-11-28 18:39:04 +0100

    New button classes + improvements

--- plugins/upload/js/directives/pictureButtonUploadDirective.js
+++ plugins/upload/js/directives/pictureButtonUploadDirective.js
@@ -31,7 +31,7 @@ angular.module('ev-upload')
             '<ev-upload settings="settings" file-success="addPicture({picture: file})"' +
                 'upload="newUpload(promise)">' +
                 '<div ng-hide="uploading">' +
-                    '<button type="button" tabIndex="-1" class="btn btn-link ev-upload-clickable"' +
+                    '<button type="button" tabIndex="-1" class="btn btn-tertiary btn-lime ev-upload-clickable"' +
                             'tooltip="{{tooltipText}}"' +
                             'tooltip-placement="top">' +
                         '<span class="icon {{iconName}}"></span>' +
