71af5ddfe916953a9baefbcb20102d77e88f849e
Christophe Hurpeau <christophe@hurpeau.com>
2015-01-02 11:55:51 +0100

    select2configuration dataProviderFilter option

--- core/js/factories/Select2Configuration.js
+++ core/js/factories/Select2Configuration.js
@@ -1,37 +1,45 @@
 /* jshint sub: true */
-angular.module('ev-fdm')
-    .factory('Select2Configuration', ['$timeout', function($timeout) {
-
+angular.module('ev-fdm').factory('Select2Configuration', [
+    '$timeout',
+    function($timeout) {
         return function(dataProvider, formatter, resultModifier, minimumInputLength, key) {
+            var dataProviderFilter;
             if (typeof dataProvider === 'object') {
                 formatter = dataProvider.formatter;
                 resultModifier = dataProvider.resultModifier;
                 minimumInputLength = dataProvider.minimumInputLength;
                 key = dataProvider.key;
                 dataProvider = dataProvider.dataProvider;
+                dataProviderFilter = dataProvider.dataProviderFilter;
+
+                if (typeof dataProviderFilter === 'object') {
+                    var filter = dataProviderFilter;
+                    dataProviderFilter = function() { return filter; };
+                } else if (typeof dataProviderFilter !== 'function') {
+                    dataProviderFilter = function() { return {}; };
+                }
             }
-            var oldQueryTerm = '',
-                filterTextTimeout;
+            var oldQueryTerm = '', filterTextTimeout;
 
             var config = {
                 minimumInputLength: angular.isDefined(minimumInputLength)
                     && angular.isNumber(minimumInputLength) ? minimumInputLength : 3,
                 allowClear: true,
                 query: function(query) {
-                    var timeoutDuration = (oldQueryTerm === query.term) ? 0 : 600;
+                    var timeoutDuration = oldQueryTerm === query.term ? 0 : 600;
 
-                        oldQueryTerm = query.term;
+                    oldQueryTerm = query.term;
 
-                        if (filterTextTimeout) {
-                            $timeout.cancel(filterTextTimeout);
-                        }
+                    if (filterTextTimeout) {
+                        $timeout.cancel(filterTextTimeout);
+                    }
 
                     filterTextTimeout = $timeout(function() {
-                        dataProvider(query.term, query.page).then(function (resources){
+                        dataProvider(query.term, query.page, dataProviderFilter).then(function(resources) {
 
                             var res = [];
-                            if(resultModifier) {
-                                angular.forEach(resources, function(resource ){
+                            if (resultModifier) {
+                                angular.forEach(resources, function(resource) {
                                     res.push(resultModifier(resource));
                                 });
                             }
@@ -40,12 +48,12 @@ angular.module('ev-fdm')
                                 results: res.length ? res : resources
                             };
 
-                            if(resources.pagination &&
+                            if (resources.pagination &&
                                 resources.pagination['current_page'] < resources.pagination['total_pages']) {
                                 result.more = true;
                             }
                             if (key && query.term.length) {
-                                var value = {id: null};
+                                var value = { id: null };
                                 value[key] = query.term;
                                 if (result.results.length) {
                                     var tmp = result.results.shift();
@@ -72,4 +80,5 @@ angular.module('ev-fdm')
             };
             return config;
         };
-    }]);
+    }
+]);
