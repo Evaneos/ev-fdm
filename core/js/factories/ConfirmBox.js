9471b413b400924d2f28c371503f36ec62d9ef2e
SelrahcD <charles@evaneos.com>
2015-02-10 16:37:03 +0100

    Template, no i18n

--- core/js/factories/ConfirmBox.js
+++ core/js/factories/ConfirmBox.js
@@ -3,18 +3,14 @@ angular.module('ev-fdm').factory('confirmBox', [
     function($modal) {
         return function(title, message, positive, negative) {
             return $modal.open({
-                backdrop: 'static', 
-                template:
-                    '<div class="modal-header">' +
-                        '<h3 class="modal-title">{{ \'' +  title  + '\'| i18n }}</h3>' +
-                    '</div>' +
-                    '<div class="modal-body">' +
-                        '{{ \'' + message + '\' | i18n }}'+
-                    '</div>' +
-                    '<div class="modal-footer">' +
-                        '<button class="btn" ng-click="$dismiss()">{{ \'' + negative + '\' | i18n }}</button>' +
-                        '<button class="btn btn-primary btn-orange" ng-click="$close()">{{ \'' + positive + '\' | i18n }}</button>' +
-                    '</div>'
+                backdrop: 'static',
+                templateUrl: 'ev-confirm-box.html',
+                controller: ['$scope', function($scope) {
+                    $scope.title    = title;
+                    $scope.message  = message;
+                    $scope.positive = positive;
+                    $scope.negative = negative;
+                }]
             }).result;
         };
     }
