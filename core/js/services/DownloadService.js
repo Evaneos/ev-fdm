3d224366df49289fb47ed6e966f580688e1ccc75
cyrilf <cyrilf7x@gmail.com>
2015-05-19 17:12:01 +0200

    Add an option to the downloadService to preprend the full host to an url

--- core/js/services/DownloadService.js
+++ core/js/services/DownloadService.js
@@ -1,14 +1,26 @@
 angular.module('ev-fdm')
-.service('DownloadService', ['$document', function($document) {
-   var iframe = null;
-   return {
-       download: function(url) {
-           if(!iframe) {
-               iframe = $document[0].createElement('iframe');
-               iframe.style.display = 'none';
-               $document[0].body.appendChild(iframe);
-           }
-           iframe.src = url;
-       }
-   };
+.service('DownloadService', ['$window', '$document', function($window, $document) {
+    var iframe = null;
+    return {
+        /**
+         * Download a specific url using an iframe
+         *
+         * @param  {string}  url         the url you want to download
+         * @param  {boolean} useFullHost either you want to prepend the full host or not (without trailing slash!)
+         */
+        download: function(url, useFullHost) {
+            if(!iframe) {
+                iframe = $document[0].createElement('iframe');
+                iframe.style.display = 'none';
+                $document[0].body.appendChild(iframe);
+            }
+
+            if(useFullHost) {
+                var fullHost = $window.location.protocol + '//' + $window.location.host;
+                url = fullHost + url;
+            }
+
+            iframe.src = url;
+        }
+    };
 }]);
